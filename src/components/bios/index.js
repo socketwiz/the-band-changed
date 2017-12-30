81db4e5df4cba2e358b3ccaa54b2d6b3c97ebe25
Ricky Nelson <rickyn@socketwiz.com>
2017-12-29 23:51:59 -0500

    Finished bios page

--- src/components/bios/index.js
+++ src/components/bios/index.js
@@ -1,4 +1,5 @@
 
+import './bios.css';
 import Main from '../../layouts/main';
 import React from 'react';
 
@@ -6,6 +7,351 @@ function BandBios() {
     return (
         <Main>
             <h1 className="title">Band Bios</h1>
+
+            <h3>Rob Talton</h3>
+            <img alt="Rob Talton"
+                className="bio-image"
+                src="/images/rob-talton.jpg" />
+            <p>
+                Rob Talton was born and raised in Orlando, Florida. Brought up by his
+                loving and supportive parents, It was there Rob developed a love for
+                music at a very young age.  His parents filled the family home with
+                music, and his love for music was nurtured by everyone in his family.
+                Rob plays the piano, keyboards, bass guitar, and guitar. 
+            </p>
+            <p>
+                &quot;My grandmother purchased something called a &#39;Chord Organ&#39;, when I was
+                7 years old...My parents were away for the weekend, and my grandmother
+                taught me to play &#39;Amazing Grace&#39; and &#39;Onward Christian Soldiers&#39;
+                before the end of the weekend.  It was my grandmother that identified
+                that I had an ear for music.  I am so grateful for the love and
+                support that I had as a child.&quot;
+            </p>
+            <p>
+                Rob began playing music professionally in 1991 in Jacksonville, Fl.
+                playing at coffee houses, bookstore cafés, and clubs as a solo
+                performer.  He also played with his Fusion band &quot;Focus&quot;.  Leading his
+                own family, Rob took a break from playing music on a professional
+                level for 15 years. 
+            </p>
+            <p>
+                &quot;A few years years ago, I will say that I had
+                something laid upon my heart, and that was to play
+                music again...music that honors God.  I felt a call to
+                form a band, made up of talented musicians whose
+                hearts were all in agreement.  We now have a great
+                band...friends, brothers, and sisters in the faith.  I
+                felt this call to take this music to the
+                streets....out in the community, where people would
+                not expect to hear music with this kind of message.
+                    Our message is Hope, Grace, Redemption, and for people
+                to know that no matter who you are, or what you&#39;ve
+                done, or where you&#39;ve come from, or where you are now,
+                there is a God that loves you and wants to change your
+                life.&quot;
+            </p>
+
+            <h3>Liz Turner</h3>
+            <img alt="Liz Turner"
+                className="bio-image"
+                src="/images/liz-turner.jpg" />
+            <p>
+                Liz grew up attending a local church. Throughout her childhood and
+                adolescence she never doubted God&#39;s existence. Though she was
+                surrounded by Godly believers, she says...
+            </p>
+            <p>
+                &quot;I never accepted him as Savior and Lord of my life until I was in
+                college. It was there, In a small, Southern Baptist Church in
+                Tallahassee Florida that my fiancé and I both received the Lord! What
+                an awesome, life changing moment that was for us!&quot;
+            </p>
+            <p>
+                Her faith and service to God has continued to grow since that time
+                too.
+            </p>
+            <p>
+                &quot;I fall in love with him all over again...every single day&quot;.
+            </p>
+            <p>
+                She serves in the ministry at the Winchester Church of God (since
+                2003) and has been a member of the choir, praise team, ensemble as
+                well as on the drama team. Liz has a gift with lyrics and writing.
+            </p>
+            <p>
+                She is self employed as a pediatric physical therapist working with
+                young children and their families in Winchester and the surrounding
+                areas. In ministry, her passion is to design, create, inspire, and
+                motivate people to Christ through music and the arts.
+            </p>
+
+            <h3>Dan May</h3>
+            <img alt="Dan May"
+                className="bio-image"
+                src="/images/dan-may.jpg" />
+            <p>
+                From Pittsburgh Pennsylvania, Dan started playing music in 1963 and
+                was totally immersed in the music culture.
+            </p>
+            <p>
+                &quot;I lived to play and the 60&#39;s and 70&#39;s were exciting times.&quot; I came
+                into a relationship with Jesus after many years of resistance. But,
+                when I made the commitment, it was deep and wide (as the kid&#39;s song
+                goes). My testimony of God&#39;s saving grace is proof that God truly does
+                love everyone!&quot;
+            </p>
+            <p>
+                Musically, he switched from those years of secular music to Jesus
+                music the moment he accepted Christ. He was radically saved.
+            </p>
+            <p>
+                &quot;I can&#39;t have my music go in a different direction than my heart. No
+                regrets, never looked back.&quot;
+            </p>
+            <p>
+                Dan has had many musical encounters throughout the years with artists
+                such as Nikki Cruz, Israel Narvez, The David Wilkerson Ministries,
+                Andréa Crouch, Dannibelle, Randy Matthews, Phil Keaggy, Kelly
+                Washington, Mark Lowery, Tom Stanko, Darrell Dement. &quot;Really, too many
+                to recall....&quot;.
+            </p>
+            <p>
+                He&#39;s been involved in countless musical projects from bands to major
+                musical productions and considers it a divine privilege to serve each
+                and every moment. When not sitting in with bands, recording or
+                jamming, he is fully engaged at his home church.
+            </p>
+            <p>
+                &quot;It&#39;s a huge blessing to proclaim the awesome gift of eternal life
+                through music&quot;.
+            </p>
+            <p>
+                He and his wife Kathy reside near Winchester, Virginia, and have been
+                married since 1971 with 2 daughters and 5 grandchildren and 1 great
+                grandchild.
+            </p>
+            <p>
+                &quot;It&#39;s great to rock with an anointed purpose&quot;.
+            </p>
+            <p>
+                Dan recommends this Instrumental CD that he contributed on with the
+                WINCOG Worship Band. Bass on all tracts except on track #9 (Where he
+                plays lead guitar).
+            </p>
+            <p>
+                <a href="http://www.amazon.com/Prelude-Worship-Winchester-Church-God/dp/B00R0MKNOO"
+                    rel="noopener noreferrer"
+                    target="_blank">
+                    http://www.amazon.com/Prelude-Worship-Winchester-Church-God/dp/B00R0MKNOO</a>
+            </p>
+
+            <h3>Aaron Adams</h3>
+            <img alt="Aaron Adoms"
+                className="bio-image"
+                src="/images/aaron-adams.jpg" />
+            <p>
+                Aaron started playing guitar at age eighteen and was greatly
+                influenced by the 60&#39;s guitar players (Clapton, Hendrix and Page). His
+                musical preference is the 90&#39;s alternative genre,
+            </p>
+            <p>
+                &quot;Because I like the variety and creativity I find there&quot;.
+            </p>
+            <p>
+                He grew up attending Stephens City, (Virginia) United Methodist as his
+                home church. It was there during his teen and college years that he
+                began attending a few other churches as well. Aaron admits that while
+                he was searching for &quot;life&#39;s meaning&quot; at one point he stopped going to
+                church altogether.
+            </p>
+            <p>
+                During that spiritual dry spell (which lasted a few years), his mother
+                convinced him to play guitar at his old home church&#39;s contemporary
+                worship service, Aaron has been faithfully doing that for many years
+                now.
+            </p>
+            <p>
+                &quot;I enjoy playing Christian contemporary because of the positive
+                message, people and opportunities that comes with the band
+                experience&quot;.
+            </p>
+            <p>
+                &quot;The Band Changed has made a big difference in my life as it has
+                allowed me to connect with God and some great individual musicians. It
+                has been a spiritual awakening and a musical reboot and recharge. I
+                needed that. I take pride in the message, music, and fellowship in The
+                Band Changed.&quot;
+            </p>
+            
+
+            <h3>Paul Crumley</h3>
+            <img alt="Paul Crumley"
+                className="bio-image"
+                src="/images/paul-crumley.jpg" />
+            <p>
+                Paul grew up in the mountains of North Carolina where his parents
+                brought him up in a mountain Baptist Church. As he grew in faith, he
+                came to accept for himself the gift of salvation through Christ&#39;s
+                death and resurrection.
+            </p>
+            <p>
+                &quot;I have found looking back that the Holy Spirit has truly been my
+                comforter in times of trouble and that God has provided life in
+                abundance&quot;.
+            </p>
+            <p>
+                His first musical exposure was listening to his parents&#39; southern
+                gospel, country music and bluegrass records. When he began to develop
+                his own taste in music, at first it was mostly Michael
+                Jackson. &quot;thankfully, I had friends who introduced me to bands like
+                Guns and Roses and Aerosmith&quot;. He started playing guitar when he was a
+                senior in high school. The school principal would allow students to
+                bring guitars and meet at lunch to trade licks. There he learned about
+                Hendrix, Led Zeppelin, and Nirvana.
+            </p>
+            <p>
+                In college he was part of a student lead Christian group and played
+                praise music for the gatherings. It was at these meetings where he met
+                his wife Rachel, who also played guitar. He also discovered Christian
+                Rock (Third Day, Jars of Clay, Switchfoot, etc.), The Allman Brothers
+                Band and Stevie Ray Vaughan.
+            </p>
+            <p>
+                &quot;At church I was blessed to play with a couple of great guitarists,
+                including my former high school principal. We played mostly bluegrass
+                and I learned a lot about rhythm guitar along the way.&quot;
+            </p>
+            <p>
+                During college, he became part of a band called Seven Strangers.
+            </p>
+            <p>
+                &quot;We played at local churches and eventually made a CD. We even took a
+                week one summer and toured the great state of North Carolina with a
+                stop or two in South Carolina.&quot;
+            </p>
+            <p>College ended and Seven Strangers ended as well. </p>
+            <p>
+                A few years later he and his wife moved to Austin, Texas, where Rachel
+                attended seminary.
+            </p>
+            <p>
+                &quot;Austin was a great town to live in as a guitar player and I enjoyed
+                seeing live music and having access to guitar stores.&quot;
+            </p>
+            <p>
+                Although he did not play out much in Austin, he started helping out
+                with sound engineering at a large church outside of town and learned a
+                lot through that experience. During his second year in Austin, his job
+                became extremely stressful and he suffered a nervous breakdown.
+            </p>
+            <p>
+                &quot;I left my job and was struggling with depression and anxiety. But God
+                surrounded me with people who supported me and helped me to heal. My
+                wife&#39;s love and support never wavered and she pushed me out of the
+                hermit hole at just the right times to keep me going.&quot;
+            </p>
+            <p>
+                They also had a neighbor at the time who was a single mother and
+                needed someone to watch her baby occasionally. He was available, so
+                she asked him to watch her daughter.
+            </p>
+            <p>
+                &quot;I honestly believe that child may have saved me in many ways because
+                she gave glimpse into what I had to look forward to in my own life, as
+                we were also expecting our first child at the time.&quot;
+            </p>
+            <p>
+                After his daughter was born, they moved back to NC for a year, and his
+                wife accepted a Pastor&#39;s position at a church in the Shenandoah Valley
+                and they moved to VA. He started playing at a new service with what
+                became unofficially known as &quot;PPR&quot;.
+            </p>
+            <p>
+                &quot;I answered a craigslist ad for a Christian band looking for a
+                guitarist and joined a Band called Seven X (yes another seven). Rob
+                Talton was also in that band which lasted for a year or so after I
+                joined.&quot;
+            </p>
+            <p>
+                After several months, Rob called him to come play with a new band he
+                was feeling lead to put together which became The Band Changed.
+            </p>
+
+            <h3>Jessica Witherspoon</h3>
+            <img alt="Jessica Witherspoon"
+                className="bio-image"
+                src="/images/jessica-witherspoon.jpg" />
+            <p>
+                Jessica remembered when she was 4 or 5 singing at church for the first
+                time and knew it was a special calling in her life. She asked Christ
+                into her life when she was very young and constantly asked for his
+                hand and guidance with everything she did. Her passion began to grow
+                when she started playing the piano and creating her own songs at a
+                young age.
+            </p>
+            <p>
+                As a teenager, she sang in the youth praise band at church and lead
+                worship on Wednesday nights. Her and her husband (Michael) met at
+                church, where he played guitar in the same band.
+            </p>
+            <p>
+                Jessica finished her education degree at St. Petersburg College in
+                Florida and shortly after that, was married to Michael. During their
+                time in Florida, they led the youth in praise and worship and also
+                taught &quot;Kids Church&quot; on Sunday mornings. Jessica and Michael moved
+                back to Winchester where Jessica is currently the librarian at Indian
+                Hollow Elementary. They have a son, Isaac, who inspires her and
+                challenges her daily to be the best mother, friend, daughter and
+                Christian.
+            </p>
+            <p>
+                During her life&#39;s journey, Jessica felt that she was not totally
+                committed to glorifying God to the fullest and was letting the
+                &quot;busyness&quot; of life get the best of her.
+            </p>
+            <p>
+                &quot;I knew that God was calling me to something greater and I was
+                frustrated because I was unsure of the next step I should take. The
+                very next day, one of the band members approached me to start singing
+                with them and I knew it was a sign from God pushing me to do what I
+                was meant to do. I thank God for every precious gift and every
+                blessing he has given me. I have so much to be thankful for and want
+                to live everyday the way he wants me to live it. Life is still busy,
+                but I know that God deserves the glory first and foremost with the
+                gifts he has entrusted me with. I am honored to be a member of this
+                band and I hope you are blessed and changed by our message&quot;
+            </p>
+
+            <h3>Ricky Nelson</h3>
+            <img alt="Ricky Nelson"
+                className="bio-image"
+                src="/images/ricky-nelson.jpg" />
+            <p>
+                Ricky grew up in the Baptist church in his home town of Leesburg,
+                Florida. In high school, he started attending the Church of God and
+                ended up going to Lee College for a year. It was there he met a friend
+                who played drums for the Lee Singers that inspired him to come home
+                that summer, purchase a set of drums and learn to play himself.  He&#39;s
+                been playing drums for the Winchester Church of God in Virginia for
+                over 20 years now. &quot;It&#39;s a blessing to be able to participate in the
+                church band with so many talented and interesting musicians all of
+                like faith.&quot;
+            </p>
+            <p>
+                He and his wife of over 20 years reside in Winchester, Virginia with
+                their 2 children. Other than playing the drums he enjoys riding
+                motorcycles, watching movies, and playing video games.
+            </p>
+            <p>
+                Ricky has had the opportunity to fill in as a backup drummer for
+                rehearsals from time to time for The Band Changed over the last couple
+                of years.  Recently an opportunity presented itself to audition for
+                the band to fill the drummer role.  Eventually he was selected to
+                become the new drummer. &quot;I&#39;m excited about this new opportunity and
+                the change of pace from church playing. I&#39;m hoping this challenge will
+                help me expand my abilities as a drummer as well as find a different
+                way to worship and make new friends.&quot;
+            </p>
         </Main>
     );
 }
