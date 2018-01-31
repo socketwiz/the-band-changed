3eacedd728d942895cefa84bd72ac496d89b1a27
Ricky Nelson <rickyn@socketwiz.com>
2018-01-31 07:47:36 -0500

    Update contact form, and event dates

--- src/containers/contact.js
+++ src/containers/contact.js
@@ -11,28 +11,15 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
     return {
-        'sendMessage': event => {
+        'sendMessage': (event, payload) => {
             event.preventDefault();
 
-            const form = event.currentTarget;
-            const name = form.querySelector('#name');
-            const email = form.querySelector('#email');
-            const updates = form.querySelector('#updates');
-            const subject = form.querySelector('#subject');
-            const message = form.querySelector('#message');
-            const payload = {
-                'name': name.value,
-                'email': email.value,
-                'updates': updates.checked,
-                'subject': subject.value,
-                'message': message.value
-            };
             const options = {
                 'method': 'POST',
                 'body': JSON.stringify(payload)
             };
 
-            return fetch('http://localhost:8080/contact', options);
+            return fetch('http://104.236.45.37/contact', options);
         }
     };
 };
