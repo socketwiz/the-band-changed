0f5e51270da407505aac9c64ff56d92f2a749ba5
Ricky Nelson <rickyn@socketwiz.com>
2018-12-24 09:01:14 -0500

    Adding new song lyrics

--- src/components/lyrics/new-day-rising.js
+++ src/components/lyrics/new-day-rising.js
@@ -12,6 +12,7 @@ function NewDayRising() {
                     <p>Blinded by faith, bound by rules</p>
                     <p>Pressin’ on every side they try to break us down</p>
                     <p>We seek the lost, the cost, too great to count</p>
+                    <p>&nbsp;</p>
                     <p>(Chorus)</p>
                     <p>We fight to stay the course, the dark one fights</p>
                     <p>to force us away</p>
@@ -20,12 +21,21 @@ function NewDayRising() {
                     <p>the Way, the Truth, the Life</p>
                     <p>(And) we’ll see the Phoenix flyin’</p>
                     <p>and a new day risin’</p>
+                    <p>&nbsp;</p>
                     <p>The enemy lurks with thirst for souls unclaimed</p>
                     <p>God sees your hurt, your work is not in vain</p>
                     <p>Let your faith arise, be bold, you’re not alone</p>
                     <p>One day soon He will reveal, oh</p>
                     <p>He’s in control</p>
+                    <p>&nbsp;</p>
                     <p>(Chorus)</p>
+                    <p>We fight to stay the course, the dark one fights</p>
+                    <p>to force us away</p>
+                    <p>The road is hard, sometimes we fall</p>
+                    <p>Gird yourself with truth, hold onto</p>
+                    <p>the Way, the Truth, the Life</p>
+                    <p>(And) we’ll see the Phoenix flyin’</p>
+                    <p>and a new day risin’</p>
                 </div>
             </div>
         </Main>
---
+++ src/components/lyrics/this-too-shall-pass.js
@@ -0,0 +1,48 @@
+
+import Main from '../../layouts/main';
+import React from 'react';
+
+function ThisTooShallPass() {
+    return (
+        <Main>
+            <h1 className="title">This Too Shall Pass</h1>
+            <div className="row">
+                <div className="col-sm-12 col-md-12 col-lg-12">
+                    <p>Faith to move mountains</p>
+                    <p>Love untamed by fear</p>
+                    <p>Together we built this home</p>
+                    <p>Our love once inspired</p>
+                    <p>Dreams shared in the dark</p>
+                    <p>Now I wake to face the days alone</p>
+                    <p>&nbsp;</p>
+                    <p>(chorus)</p>
+                    <p>How did we get where we are</p>
+                    <p>For better or worse, but worse so far</p>
+                    <p>As one we&#39;ve faced trials in the past (we&#39;ve faced the storms together in the past)</p>
+                    <p>Help us to get back</p>
+                    <p>Cuz I know this love can last</p>
+                    <p>And I know this too shall pass</p>
+                    <p>&nbsp;</p>
+                    <p>I look at the faces</p>
+                    <p>Of a thousand memories</p>
+                    <p>And all of the promises we&#39;ve kept</p>
+                    <p>But time&#39;s become burden</p>
+                    <p>With my feet held to the flame</p>
+                    <p>All I&#39;ve got to give is what&#39;s left</p>
+                    <p>&nbsp;</p>
+                    <p>(chorus)</p>
+                    <p>How did we get where we are</p>
+                    <p>For better or worse, but worse so far</p>
+                    <p>As one we&#39;ve faced trials in the past (we&#39;ve faced the storms together in the past)</p>
+                    <p>Help us to get back</p>
+                    <p>Cuz I know this love can last</p>
+                    <p>And I know this too shall pass</p>
+                </div>
+            </div>
+        </Main>
+    );
+}
+
+export default ThisTooShallPass;
+
+
